2045649f60b9c505819bd7bc46a0b74baccc47f1
brecht <brecht.baeten@gmail.com>
2016-05-31 09:08:09 +0200

    updated plotting example

--- examples/numpy_scipy_matplotlib.py
+++ examples/numpy_scipy_matplotlib.py
@@ -2,22 +2,31 @@ import numpy as np
 import scipy.integrate
 import matplotlib.pyplot as plt
 
-x = np.linspace(0,2*np.pi,100)
-c = np.cos(x)
+x = np.linspace(0,2*np.pi,50)
+
+# build an array elementwise
+c = np.zeros_like(x)
+for i,xi in enumerate(x):
+	c[i] = np.cos(xi)
+
+# vectorized functions
 s = np.sin(x)
+
 ss = scipy.integrate.cumtrapz(c,x)
 
+# plotting parameters
 plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+plt.rc('font', family='serif', size=8)
 plt.rc('figure', autolayout=True)
 
-plt.figure(figsize=(15/2.54,10/2.54))
+# plotting
+plt.figure(figsize=(10/2.54,7/2.54))
 plt.plot(x,c,label=r'cosinus')
-plt.plot(x[1:],ss,label=r'$\int$ cosinus$(x)$ d$x$')
-plt.plot(x,s,label=r'sinus')
+plt.plot(x,s,'-s',label=r'sinus')
+plt.plot(x[1:],ss,label=r'$\int$ cosinus$(x)$ d$x$',linewidth=2)
 plt.gca().set_xlabel(r'$x$ (rad)')
 plt.gca().set_ylabel(r'$y$')
-plt.legend()
+plt.legend(numpoints=1)
 
 plt.savefig('sinus_cosinus.pdf')
 plt.show()
